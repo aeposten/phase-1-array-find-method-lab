83ccf9b274d84b869e47de27930aace1bdaee5aa
Amy Posten <amyposten85@gmail.com>
2021-09-27 10:13:36 -0400

    commit tree 5cd6b501cfbedda5e67a3e88e5ffcd9087762b83

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+//write a function called superBowlWin
+superbowlWin = (array) => { // it should receive 1 argument, an array of JS objects, each obj has 2 props year & result
+    let result = array.find(array => array.result === 'W' );
+        return result ? result.year : undefined;
+}
+    // it should use find to test each obj and see if the result is "W"
 
+//it should return the year when the win occured, if no win is found return undefined
